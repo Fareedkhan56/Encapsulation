0af2f81ead5c0bb569b0664b91ff722083fb2305
Fareed Khan <127206523+Fareedkhan56@users.noreply.github.com>
2023-10-13 19:03:44 +0500

    Add files via upload

---
+++ encapsulation/encapsulation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation
+{
+    class Account
+    {
+        public int accountBalance = 1000;
+
+        public int Balance
+        {
+            set
+            {
+                if(value < 0)
+                {
+                    Console.WriteLine("you can nit add negative value");
+                }
+                else
+                {
+                    accountBalance = accountBalance + value;
+                }
+            }
+            get
+            {
+                return accountBalance;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+            account.Balance = Convert.ToInt32 (Console.ReadLine()) ;
+            Console.WriteLine(account.Balance);
+            Console.ReadLine();
+        }
+    }
+}
